dc4c24f46f62c7899219f66961fdcafcfcf5aac4
a05annex <a05annex@gmail.com>
2019-01-24 15:04:52 -0800

    removed some other comment

--- frc 2019/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ frc 2019/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -10,7 +10,6 @@ import frc.robot.RobotMap;
 public class DriveTrain extends Subsystem{
 
     public WPI_TalonSRX
-        //this is a comment to test branches
         rightMotor = new WPI_TalonSRX(RobotMap.rm1),
         rm2 = new WPI_TalonSRX(RobotMap.rm2),
         rm3 = new WPI_TalonSRX(RobotMap.rm3),
