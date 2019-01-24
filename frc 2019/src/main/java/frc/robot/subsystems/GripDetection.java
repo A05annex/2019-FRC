355700655880fc9119c8373e3d55d10203092a63
GraciousProfishin-alism <thedarkjarl@gmail.com>
2019-01-23 19:25:45 -0800

    i added grip stuff that doesn't do much quite yet. need a drivetrain behavior that takes a double[] var with two doubles first being the speed of motor left, second being motor right speed

---
+++ frc 2019/src/main/java/frc/robot/subsystems/GripDetection.java
@@ -0,0 +1,125 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.GripPipeline;
+
+import java.sql.Array;
+
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.vision.VisionRunner;
+import edu.wpi.first.wpilibj.vision.VisionThread;
+
+/**
+ * Add your docs here.
+ */
+public class GripDetection extends Subsystem {
+	
+	public static final int IMG_WIDTH = 320;
+	public static final int IMG_HEIGHT = 240;
+	
+	private VisionThread visionThread;
+  private double centerX1 = 0.0;
+  private double centerX2 = 0.0;
+  private double centerY1 = 0.0;
+  private double centerY2 = 0.0;
+  private double Height1;
+  private double Height2;
+  private double Width1;
+  private double Width2;
+  private double[] coords1 = new double[]{0.0,0.0};
+  private double[] coords2 = new double[]{0.0,0.0};
+  UsbCamera camera;
+  double[] motorPower = new double[]{0,0};
+	
+	private final Object imgLockCX1 = new Object();
+	private final Object imgLockCX2 = new Object();
+	private final Object imgLockCY1 = new Object();
+  private final Object imgLockCY2 = new Object();
+  
+	private final Object imgLockCW1 = new Object();
+	private final Object imgLockCW2 = new Object();
+	private final Object imgLockCH1 = new Object();
+	private final Object imgLockCH2 = new Object();
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  public void startGrip(){
+    camera = CameraServer.getInstance().startAutomaticCapture();
+    camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
+    
+    visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> {
+        if (!pipeline.filterContoursOutput().isEmpty()) {
+            Rect r1 = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+            Rect r2 = Imgproc.boundingRect(pipeline.filterContoursOutput().get(1));
+            synchronized (imgLockCX1) {centerX1 = r1.x + (r1.width / 2);}
+            synchronized (imgLockCX2) {centerX2 = r2.x + (r2.width / 2);}
+            synchronized (imgLockCY1) {centerY1 = r1.y + (r1.height / 2);}
+            synchronized (imgLockCY2) {centerY2 = r2.y + (r2.height / 2);}
+
+            synchronized (imgLockCW1) {Width1 = r1.width;}
+            synchronized (imgLockCW2) {Width2 = r2.width;}
+            synchronized (imgLockCH1) {Height1 = r1.height;}
+            synchronized (imgLockCH2) {Height2 = r2.height;}
+        }
+      });
+    visionThread.start();
+
+  }
+  private double[] sendWidthHeight1(){
+    double Width1;
+    double Height1;
+    synchronized (imgLockCW1) {Width1=this.Width1;}
+    synchronized (imgLockCH1) {Height1=this.Height1;}
+    double[] WidthHeight1 = new double[]{Width1,Height1};
+    return WidthHeight1;
+  }
+  private double[] sendWidthHeight2(){
+    double Width2;
+    double Height2;
+    synchronized (imgLockCW2) {Width2=this.Width2;}
+    synchronized (imgLockCH2) {Height2=this.Height2;}
+    double[] WidthHeight1 = new double[]{Width2,Height2};
+    return WidthHeight1;
+  }
+  private double[] sendXY1(){
+    double centerX1;
+    double centerY1;
+    synchronized (imgLockCX1){centerX1=this.centerX1;}
+    synchronized (imgLockCY1){centerY1=this.centerY1;}
+    coords1[0]=centerX1;
+    coords1[1]=centerY1;
+    return(coords1);
+  }
+  private double[] sendXY2(){
+    double centerX2;
+    double centerY2;
+    synchronized (imgLockCX2){centerX2=this.centerX2;}
+    synchronized (imgLockCY2){centerY2=this.centerY2;}
+    coords2[0]=centerX2;
+    coords2[1]=centerY2;
+    return(coords2);
+  }
+  public double[] straightenOut(){
+    motorPower[0]=0;
+    motorPower[1]=0;
+    return(motorPower);
+
+  }
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
