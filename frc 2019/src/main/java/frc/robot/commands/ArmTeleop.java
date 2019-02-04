25c56fdf85837b4846cc7782fd0e32732084dce2
roy <rhall@rubota.com>
2019-02-03 17:02:00 -0800

    Cleanup on arm subssytem, added framework for arm movement constraints, made command and subsystem follow MVC conventions. Drive cleanup and documentation.

--- frc 2019/src/main/java/frc/robot/commands/ArmTeleop.java
+++ frc 2019/src/main/java/frc/robot/commands/ArmTeleop.java
@@ -2,44 +2,62 @@ package frc.robot.commands;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
+/**
+ * This command runs the arm from the driver station using buttons on the joystick for testing.
+ */
 public class ArmTeleop extends Command{
 
     public ArmTeleop() {
-
-        //only functions if the drive train is not in use by another command
+        // This command can only function if the robot arm is NOT being controlled by something else.
         requires(Robot.armDriveTrain);
       }
     
       @Override
       protected void initialize() {
-          //sets the wheels to brake when assigned a motor power of 0
+        // Sets the arms to brake when assigned a power of 0.0 (does this
+        // apply when you don't have encoders?)
         Robot.armDriveTrain.setNeutralMode(NeutralMode.Brake);
       }
     
       @Override
       protected void execute() {
-          //runs the arcadeDrive function from the drive train
-          Robot.armDriveTrain.stickDrive(Robot.oi.getStick());
+        Joystick stick = Robot.oi.getStick();
+        double lowerArmPower = 0.0;
+        double upperArmPower = 0.0;
+        // Adjust the arm power if any arm power controls are activated.
+        if(stick.getRawButton(5)){
+            lowerArmPower = 0.3;
+
+        }
+        else if(stick.getRawButton(6)){
+            lowerArmPower = -0.3;
+        }
+        // TODO: add upper arm controls
+
+        // set the arm lower and upper power components
+        Robot.armDriveTrain.setArmPower(lowerArmPower, upperArmPower);
       }
     
       @Override
       protected boolean isFinished() {
-        //only returns false because the command cannot end without being interrupted
+        // Returns false because the command cannot end without being interrupted
         return false;
       }
     
       @Override
       protected void end() {
-          //calls function to stop the motors
+          // Calls function to stop the motors
           Robot.armDriveTrain.stop();
       }
     
       @Override
       protected void interrupted() {
-          //runs the end method when another command requests use of the drivetrain
+          // Runs the end method when another command requests use of the robot arm.
           end();
       }
 }
--- frc 2019/src/main/java/frc/robot/subsystems/ArmDriveTrain.java
+++ frc 2019/src/main/java/frc/robot/subsystems/ArmDriveTrain.java
@@ -3,16 +3,40 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 import frc.robot.commands.ArmTeleop;
 
+/**
+ * The robot arm drive subsystem. This subsystem is controlling the lower arm motor
+ * and the upper arm motor only. There are position potentiometers for the lower and
+ * upper arms controlled by those motors. The controll of the head/bucket is
+ * independent of the arm.
+ * 
+ * Roy's note: I'm thinking the arm drive subsystem knows about the potentiometers
+ * and the reporting of arm position, and that the control happens in the commands
+ * initially - some control method may move to the arm once we really know how to
+ * control them.
+ */
 public class ArmDriveTrain extends Subsystem{
-    public WPI_TalonSRX
-        armMotorLower = new WPI_TalonSRX(RobotMap.arm1),
-        armMotorUpper = new WPI_TalonSRX(RobotMap.arm2);
+
+    public WPI_TalonSRX armMotorLower = new WPI_TalonSRX(RobotMap.arm1);
+    public WPI_TalonSRX armMotorUpper = new WPI_TalonSRX(RobotMap.arm2);
+    // TODO: once the arms and done and the positions of the potentiometers are fixed, manually
+    // rotate the arms and track the potentiometer values at the limits of motion. These become
+    // the constraints for arm movement - i.e. if you try to move the arm beyond these values
+    // you will run into the frame or some other hard stop that could damage the robot/arm - don't
+    // let that happen !!
+    private double lowerArmMin = 0.0;
+    private double lowerArmMax = 5.0;
+    private double upperArmMin = 0.0;
+    private double upperArmMax = 5.0;
+    private double armStopBuffer = 0.25;    // The distance before the hard stop that you should
+                                            // cut power to 0.0
+    private double armCreepBuffer = 0.50;   // The distance before the hard stop that you
+                                            // should cut power to creep power
+    private double armCreepPower = 0.1;     // The maximum power in the creep zone
+
     
     public ArmDriveTrain(){
         //constructs and configures all six drive motors
@@ -23,20 +47,64 @@ public class ArmDriveTrain extends Subsystem{
     public void initDefaultCommand(){
         setDefaultCommand(new ArmTeleop());
     }
-    public void stickDrive(Joystick stick){
-        if(stick.getRawButton(5)){
-            armMotorLower.set(.3);
 
-        }
-        else if(stick.getRawButton(6)){
-            armMotorLower.set(-.3);
-        }
-        else{
-            armMotorLower.set(0);
-        }
+    /**
+     * The position of the lower arm in the range <tt>lowerArmMin</tt> to <tt>lowerArmMax</tt>
+     * @return (double) The position of the lower arm.
+     */
+    public double getLowerArmPosition() {
+        // TODO: Map from the potentiometer to some position. This could just be the
+        // potentiometer value, or it could be mapped to a 'more meaningful' value
+        // like degrees from horizontal.
+        return 2.5;
     }
 
-    public void inputDrive(double[] motorInput){
+    /**
+     * The position of the upper arm in the range <tt>upperArmMin</tt> to <tt>upperArmMax</tt>
+     * @return (double) The position of the upper arm.
+     */
+    public double getUpperArmPosition() {
+        // TODO: Map from the potentiometer to some position. This could just be the
+        // potentiometer value, or it could be mapped to a 'more meaningful' value
+        // like degrees from horizontal.
+        return 2.5;
+    }
+
+    /**
+     * Set the arm motor powers for the lower and upper arms.
+     * @param lowerArmPower (double) The power to the lower arm in the range -1 to 1.
+     * @param upperArmPower (double) The power to the upper arm in the range -1 to 1.
+     */
+    public void setArmPower( double lowerArmPower, double upperArmPower ) {
+        // TODO: check lower arm power direction, test against arm position and/or limit
+        // switch and set to 0 if we have hit the constraint for that direction
+        if (lowerArmPower < 0.0) {
+            if (getLowerArmPosition() < (lowerArmMin + armCreepBuffer)) {
+                lowerArmPower = (getLowerArmPosition() < (lowerArmMin + armStopBuffer)) ?
+                    0.0 : armCreepPower;
+            }
+        } else if (lowerArmPower > 0.0) {
+            if (getLowerArmPosition() > (lowerArmMax - armCreepBuffer)) {
+                lowerArmPower = (getLowerArmPosition() > (lowerArmMax - armStopBuffer)) ?
+                    0.0 : armCreepPower;
+            }
+        }
+        // TODO: check upper arm power direction, test against arm position and/or limit
+        // switch and set to 0 if we have hit the constraint for that direction
+        if (upperArmPower < 0.0) {
+            if (getLowerArmPosition() < (upperArmMin + armCreepBuffer)) {
+                upperArmPower = (getLowerArmPosition() < (upperArmMin + armStopBuffer)) ?
+                    0.0 : armCreepPower;
+            }
+        } else if (upperArmPower > 0.0) {
+            if (getUpperArmPosition() > (upperArmMax - armCreepBuffer)) {
+                upperArmPower = (getUpperArmPosition() > (upperArmMax - armStopBuffer)) ?
+                    0.0 : armCreepPower;
+            }
+        }
+        // now that we have conditioned the power to the constraints, set the power
+        armMotorLower.set(lowerArmPower);
+        armMotorUpper.set(upperArmPower);
     }
 
     public void setNeutralMode(NeutralMode mode){
