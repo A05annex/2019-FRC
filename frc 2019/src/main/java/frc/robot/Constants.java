f1d3c43a14408bff0d8e3ee59391e9406a5f04f6
a05annex <a05annex@gmail.com>
2019-03-08 15:39:39 -0800

    added lift constants

--- frc 2019/src/main/java/frc/robot/Constants.java
+++ frc 2019/src/main/java/frc/robot/Constants.java
@@ -26,8 +26,10 @@ public class Constants {
     public static final double TARGET_POSITION_TOLERANCE = 5.0;
 
 
-    public static final double END_GAME_PNEUMATICS_LIFT_DURATION = 3.0;
+    public static final double END_GAME_PNEUMATICS_LIFT_DURATION = 15.0;
     public static final double END_GAME_PNEUMATICS_RETRACT_DURATION = 4.0;
+    public static final double END_GAME_PNEUMATICS_DROP = 5.0;
+    
     public static final double END_GAME_MOTOR_POWER = 0.7;
     public static final double END_GAME_DRIVE_DURATION = 4.0;
     public static final int INTERPOLATE_STEPS = 50;
--- frc 2019/src/main/java/frc/robot/commandgroups/LiftAndDuringLift.java
+++ frc 2019/src/main/java/frc/robot/commandgroups/LiftAndDuringLift.java
@@ -8,7 +8,9 @@
 package frc.robot.commandgroups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Constants;
 import frc.robot.commands.ArmInterpolateToTarget;
+import frc.robot.commands.ArmTimedToTarget;
 import frc.robot.commands.SynchronisedLift;
 import frc.robot.subsystems.ArmPositions;
 
@@ -19,7 +21,8 @@ public class LiftAndDuringLift extends CommandGroup {
     public LiftAndDuringLift() {
 
     //activates pneumatics as arm helps pull robot onto platform
-    addParallel(new ArmInterpolateToTarget(ArmPositions.DURING_LIFT));
+    addParallel(new ArmTimedToTarget(ArmPositions.DURING_LIFT,Constants.END_GAME_PNEUMATICS_DROP,
+    Constants.END_GAME_PNEUMATICS_LIFT_DURATION));
     addSequential(new SynchronisedLift());
 
     //will need to retest with new pneumatics. timing may be off.
