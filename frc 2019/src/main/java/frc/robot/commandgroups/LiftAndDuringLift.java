f624f45036daf9c534329df82d4b6b1e1967d060
roy <rhall@rubota.com>
2019-03-15 08:46:26 -0700

    Set initial arm position in telop and autonomous.

--- frc 2019/src/main/java/frc/robot/commandgroups/LiftAndDuringLift.java
+++ frc 2019/src/main/java/frc/robot/commandgroups/LiftAndDuringLift.java
@@ -9,23 +9,41 @@ package frc.robot.commandgroups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Constants;
+import frc.robot.Robot;
 import frc.robot.commands.ArmTimedToTarget;
 import frc.robot.commands.LiftingPower;
 import frc.robot.commands.SynchronisedLift;
 import frc.robot.subsystems.ArmPositions;
 
 public class LiftAndDuringLift extends CommandGroup {
+
+//    boolean isFinished = false;
+//    // Can be overwritten by teams
+//    @Override
+//    protected void initialize() {
+//        // if we are not at pre-lift position we shouyld not be lifting.
+//        isFinished = ArmPositions.PRE_ENDGAME_LIFT != Robot.armDriveTrain.getTargetPosition();
+//    }
+//
+//    @Override
+//    protected boolean isFinished() {
+//        if (isFinished) {
+//            return true;
+//        }
+//        return super.isFinished();
+//    }
+
     /**
      * Add your docs here.
      */
     public LiftAndDuringLift() {
 
-    //activates pneumatics as arm helps pull robot onto platform
-    addSequential(new LiftingPower(true));
-    addSequential(new InterpolateAndCheck(ArmPositions.START_LIFT));
-    addParallel(new ArmTimedToTarget(ArmPositions.DURING_LIFT,Constants.END_GAME_PNEUMATICS_DROP,
-    Constants.END_GAME_PNEUMATICS_LIFT_DURATION));
-    addSequential(new SynchronisedLift());
+        //activates pneumatics as arm helps pull robot onto platform
+        addSequential(new LiftingPower(true));
+        addSequential(new InterpolateAndCheck(ArmPositions.START_LIFT));
+        addParallel(new ArmTimedToTarget(ArmPositions.DURING_LIFT,Constants.END_GAME_PNEUMATICS_DROP,
+        Constants.END_GAME_PNEUMATICS_LIFT_DURATION));
+        addSequential(new SynchronisedLift());
     }
 }
 
